2a90a316d74f06219997e567f9e2da8aad4c920e
Lazared <eduard.enl@gmail.com>
2024-01-22 15:11:59 +0000

    tracking link changed

--- app/routes/webhooks.jsx
+++ app/routes/webhooks.jsx
@@ -72,7 +72,7 @@ export const action = async ({ request }) => {
       `mutation {
         fulfillmentTrackingInfoUpdateV2(
           fulfillmentId: "gid://shopify/Fulfillment/${payload.fulfillments[0].id}"
-          trackingInfoInput: {number: "${number}", company: "AddLee Now", url: "https://www.addisonlee.com/"}
+          trackingInfoInput: {number: "${number}", company: "AddLee Now", url: "http://trackdriver.addisonlee.com/y47hfqrOqEYIJwOH"}
         ) {
           userErrors {
             field
